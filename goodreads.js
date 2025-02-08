a1acf7980a97bc7e5a9c1dd2da95ad33c78d561f
Hariprasath V <144115008+Hariprasath1410@users.noreply.github.com>
2025-02-08 19:49:26 +0530

    Update goodreads.js

--- goodreads.js
+++ goodreads.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const path = require('path')
+
 const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
 const bcrypt = require('bcrypt')
 
+const jwt = require('jsonwebtoken')
 const app = express()
 app.use(express.json())
+
 const dbPath = path.join(__dirname, 'goodreads.db')
 
 let db = null
@@ -27,50 +30,69 @@ const initializeDBAndServer = async () => {
 initializeDBAndServer()
 
 // Get Books API
-app.get('/books/', async (request, response) => {
-  const getBooksQuery = `
-  SELECT
-    *
-  FROM
-    book
-  ORDER BY
-    book_id;`
-  const booksArray = await db.all(getBooksQuery)
-  response.send(booksArray)
+app.get('/books/', (request, response) => {
+  let jwtToken
+  const authHeader = request.headers['authorization']
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(' ')[1]
+  }
+  if (jwtToken === undefined) {
+    response.status(401)
+    response.send('Invalid Access Token')
+  } else {
+    jwt.verify(jwtToken, 'MY_SECRET_TOKEN', async (error, payload) => {
+      if (error) {
+        response.send('Invalid Access Token')
+      } else {
+        const getBooksQuery = `
+            SELECT
+              *
+            FROM
+             book
+            ORDER BY
+              book_id;`
+        const booksArray = await db.all(getBooksQuery)
+        response.send(booksArray)
+      }
+    })
+  }
 })
-
+// User Register API
 app.post('/users/', async (request, response) => {
-  const {username, name, gender, password, location} = request.body
+  const {username, name, password, gender, location} = request.body
   const hashedPassword = await bcrypt.hash(password, 10)
   const selectUserQuery = `
-  SELECT * FROM user WHERE username = '${username}';
-  `
+    SELECT 
+      * 
+    FROM 
+      user 
+    WHERE 
+      username = '${username}';`
   const dbUser = await db.get(selectUserQuery)
   if (dbUser === undefined) {
     const createUserQuery = `
-  INSERT INTO
-    user (username, name, password, gender, location)
-  VALUES
-    (
-      '${username}',
-      '${name}',
-      '${hashedPassword}',
-      '${gender}',
-      '${location}'  
-    );`
+     INSERT INTO
+      user (username, name, password, gender, location)
+     VALUES
+      (
+       '${username}',
+       '${name}',
+       '${hashedPassword}',
+       '${gender}',
+       '${location}'  
+      );`
     await db.run(createUserQuery)
-    response.send('User Created Successfully.')
+    response.send('User created successfully')
   } else {
     response.status(400)
-    response.send('User Name Already Exists.')
+    response.send('User already exists')
   }
 })
 
-app.post('/login/', async (request, response) => {
+// User Login API
+app.post('/login', async (request, response) => {
   const {username, password} = request.body
-  const selectUserQuery = `
-  SELECT * FROM user WHERE username = '${username}';
-  `
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`
   const dbUser = await db.get(selectUserQuery)
   if (dbUser === undefined) {
     response.status(400)
@@ -78,7 +100,11 @@ app.post('/login/', async (request, response) => {
   } else {
     const isPasswordMatched = await bcrypt.compare(password, dbUser.password)
     if (isPasswordMatched === true) {
-      response.send('Login Success')
+      const payload = {
+        username: username,
+      }
+      const jwtToken = jwt.sign(payload, 'MY_SECRET_TOKEN')
+      response.send({jwtToken})
     } else {
       response.status(400)
       response.send('Invalid Password')
